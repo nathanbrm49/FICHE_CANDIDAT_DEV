4dc0dcddd9680eb7822ea5be66a8b5d1b165f038
NathanBrillaultMery <nathan.bm.formation@gmail.com>
2022-04-19 16:22:05 +0200

    correction1

--- common/header.php
+++ common/header.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- <link rel="stylesheet" href="./public/main.css"> -->
     <link rel="stylesheet" href="./public/css/fiche.css">
 
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
