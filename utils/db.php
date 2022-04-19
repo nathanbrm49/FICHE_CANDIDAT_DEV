aa718d18e163d99072d3236feba645851c476769
NathanBrillaultMery <nathan.bm.formation@gmail.com>
2022-04-19 16:27:30 +0200

    changement motdepasse

--- utils/db.php
+++ utils/db.php
@@ -3,14 +3,14 @@
 $server = "localhost";
 $db = "fiche_greta";
 $user = "root";
-$passwd = "";
+$secret = "";
 // Fin de la déclaration des paramètres
 
 // Cette partie est générique à l'ensemble de vos projets utilisant une base de données.
 $dsn = "mysql:host=$server;dbname=$db;charset=utf8";
 
 try {
-    $pdo = new PDO($dsn, $user, $passwd);
+    $pdo = new PDO($dsn, $user, $secret);
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $e) {
